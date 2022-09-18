3bfc0cfc654f1ca2fe43948c283da0ac43f56f58
Amanda Sharmaine Remo <asremo@Amandas-MBP.lan>
2022-09-18 14:19:41 -0400

    finished commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+
+function SearchBar(props){
+    // We can comment out our searchTerm state variable as we are not using it!
+    // let [searchTerm, setSearchTerm] = useState('')
+
+    return (
+        <form className='App'>
+            <input required type="text" placeholder="Enter a search term here"
+                onChange={
+                    (e) => props.handleSearch(e, e.target.value)
+                } />
+                <p>&nbsp;</p>
+        </form>
+    )
+}
+
+
+export default SearchBar
